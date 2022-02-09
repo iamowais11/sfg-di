661d3891c573d9a16790f2c19d6d67d4e87b644c
iamowais11 <owaisdeveloper2021@gmail.com>
2022-02-09 11:44:07 +0530

    initialcommit

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -1,13 +1,18 @@
 package com.example.sfgdi;
 
+import com.example.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext context=SpringApplication.run(SfgDiApplication.class, args);
+		MyController controller=(MyController) context.getBean("myController");
+		String greetings=controller.sayHello();
+		System.out.println(greetings);
 	}
 
 }
